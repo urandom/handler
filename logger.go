1cf351f682568991bb6446ff85cd99ea7c70a9bc
Viktor Kojouharov <developer@sugr.org>
2016-10-09 11:58:28 +0300

    Add docs to the various simple loggers

--- logger.go
+++ logger.go
@@ -10,16 +10,19 @@ type Logger interface {
 	Print(v ...interface{})
 }
 
+// NopLogger is a Logger implementation that does nothing
 type NopLogger struct{}
 
 func (l NopLogger) Print(v ...interface{}) {}
 
+// OutLogger is a Logger implementation that outputs to os.Stdout
 type OutLogger struct{}
 
 func (l OutLogger) Print(v ...interface{}) {
 	fmt.Fprint(os.Stdout, v...)
 }
 
+// ErrLogger is a Logger implementation that outputs to os.Stderr
 type ErrLogger struct{}
 
 func (l ErrLogger) Print(v ...interface{}) {
